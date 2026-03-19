c9f0d76c7cb5baecaabbf492909e1f2523f6434c
danielbilkvist <danielbilkvist@gmail.com>
2026-03-19 19:29:54 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CoilQuiz/',
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  base: "/CoilQuiz/",
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
